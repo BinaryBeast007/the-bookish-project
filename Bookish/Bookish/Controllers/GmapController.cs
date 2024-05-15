c731c5c39cbdfcdf1855bb386b636ff44baaeaf7
Farzana Rahman Oishee <113884900+Oishee01@users.noreply.github.com>
2024-05-15 10:24:27 +0600

    initial commit

---
+++ Bookish/Bookish/Controllers/GmapController.cs
@@ -0,0 +1,31 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Bookish.Controllers
+{
+    public class GmapController : ApiController
+    {
+        [HttpGet]
+        [Route("api/gmap/{from}/{to}")]
+        public HttpResponseMessage GetGoogleMapsLink(string from, string to)
+        {
+            try
+            {
+                var url = $"https://www.google.com/maps/dir/{from}/{to}";
+                var response = Request.CreateResponse(HttpStatusCode.OK, url);
+                return response;
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = Request.CreateErrorResponse(HttpStatusCode.InternalServerError, $"Error: {ex.Message}");
+                return errorResponse;
+            }
+        }
+    }
+}
